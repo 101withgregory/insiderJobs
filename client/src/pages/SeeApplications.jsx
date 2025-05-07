a99d3cd6586d9cae67d9d0be38ec435b6ffa522a
101withgregory <101withgregoryonyando@gmail.com>
2025-05-07 16:21:13 +0300

    fixed an error

--- client/src/pages/SeeApplications.jsx
+++ client/src/pages/SeeApplications.jsx
@@ -42,7 +42,7 @@ function SeeApplications() {
             fetchCompanyjobApplications()
         }
     }, [companyToken])
-    return applicants ? applicants.length !== 0 ? (<div className='flex items-center justify-center h-[70vh]'>
+    return applicants ? applicants.length === 0 ? (<div className='flex items-center justify-center h-[70vh]'>
         <p className='text-xl sm:text-2xl'>No Applications Available</p>
       </div>) : (
         <div className='container mx-auto p-4'>
